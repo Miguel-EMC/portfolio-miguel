2576002109f87c3d51007ccb0f51e41707433eb7
EMC-Miguel <eduardomuzo123455@gmail.com>
2025-12-06 13:10:16 -0500

    Configuration workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'protafolio-personal' title`, () => {
+  it(`should have the 'portfolio-personal' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('protafolio-personal');
+    expect(app.title).toEqual('portfolio-personal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, protafolio-personal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio-personal');
   });
 });
