94fc5a78695efa169f1f84a35cd6dc34a205720f
EMC-Miguel <eduardomuzo123455@gmail.com>
2026-06-30 23:23:52 -0500

    feat: enhance avatar display and footer functionality with scroll-to-top button

--- apps/portfolio/src/app/shared/components/layout/footer/footer.component.ts
+++ apps/portfolio/src/app/shared/components/layout/footer/footer.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, NgClass } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  showScrollButton = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.showScrollButton = window.pageYOffset > 300;
+    }
+  }
+
   scrollToTop(): void {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
   }
 }
